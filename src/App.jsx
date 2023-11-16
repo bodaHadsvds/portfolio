a5ca8e1323f8f83a6fdd4b3ac79f7e4d4c256a70
unknown <abdelrahmanmustafa088@gmail.com>
2023-11-16 12:19:37 +0200

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Skills  from './components/skills'
 import Skillscard from './components/skillscard'
 import About from './components/about'
 import Projects from './components/projects'
-import Projectcard from './components/projectcard'
-
 const App = () => {
   return (
     <div>
